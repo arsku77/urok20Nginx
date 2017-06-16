568f1b1a8ac232d541c09b9144fbd1c43194180c
Arunas Skucas <arsku77@gmail.com>
2017-06-16 07:36:39 +0000

    urok13 better

--- frontend/views/clean-blog/layouts/main.php
+++ frontend/views/clean-blog/layouts/main.php
@@ -50,8 +50,32 @@ NavBar::begin([//atidarome navigacinį meniu
     'brandLabel' => 'My Company',
     'brandUrl' => Yii::$app->homeUrl,
     'options' => [
-        'style' => 'background-color: #660000',
-        'class' => 'navbar navbar-default navbar-custom navbar-fixed-top',
+      'class' => 'navbar navbar-collapse navbar-custom navbar-fixed-top',
+          'div' => [
+          'class' => 'collapse navbar-collapse',
+          'id' => 'bs-example-navbar-collapse-1',
+            'div' => [
+            'class' => 'container-fluid',
+                'div' => [
+                'class' => 'navbar-header page-scroll',
+                    'button' => [
+                    'type' => 'button',
+                    'class' => 'navbar-toggl',
+                    'data-toggle' => 'collapse',
+                    'data-targe' => '#bs-example-navbar-collapse-1',
+                            'span' => [
+                            'class' => 'sr-only',
+                            'Toggle navigation',
+                            ],
+                    'Menu',
+                        'i' => [
+                        'class' => 'fa fa-bars',
+                        ],
+                    ],
+
+                ],
+            ],
+        ],
     ],
 ]);
 
@@ -76,55 +100,17 @@ if (Yii::$app->user->isGuest) {
         . '</li>';
 }
 echo Nav::widget([
-    'options' => ['class' => 'navbar-nav navbar-right'],
+    'options' => ['class' => 'nav navbar-nav navbar-right',],
     'items' => $menuItems,
 ]);
 NavBar::end();//uždarom meniu juostą
 
 
 ?>
-<!--    <nav class="navbar navbar-default navbar-custom navbar-fixed-top">-->
-<!--        <div class="container-fluid">-->
-<!--            <!-- Brand and toggle get grouped for better mobile display -->-->
-<!--            <div class="navbar-header page-scroll">-->
-<!--                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">-->
-<!--                    <span class="sr-only">Toggle navigation</span>-->
-<!--                    Menu <i class="fa fa-bars"></i>-->
-<!--                </button>-->
-<!--                <a class="navbar-brand" href="index.html">Start Bootstrap</a>-->
-<!--            </div>-->
-<!---->
-<!---->
-<!---->
-<!---->
-<!--            <!-- Collect the nav links, forms, and other content for toggling -->-->
-<!--            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">-->
-<!--                <ul class="nav navbar-nav navbar-right">-->
-<!--                    <li>-->
-<!--                        <a href="index.html">Home</a>-->
-<!--                    </li>-->
-<!--                    <li>-->
-<!--                        <a href="about.html">About</a>-->
-<!--                    </li>-->
-<!--                    <li>-->
-<!--                        <a href="post.html">Sample Post</a>-->
-<!--                    </li>-->
-<!--                    <li>-->
-<!--                        <a href="contact.html">Contact</a>-->
-<!--                    </li>-->
-<!--                </ul>-->
-<!--            </div>-->
-<!--            <!-- /.navbar-collapse -->-->
-<!--        </div>-->
-<!--        <!-- /.container -->-->
-<!--    </nav>-->
-<!---->
-<!---->
-
 
     <!-- Page Header -->
     <!-- Set your background image for this header on the line below. -->
-    <header class="intro-header" style="background-image: url('files/themes/clean-blog/photos/gallery/post-bg.jpg')">
+    <header class="intro-header" style="background-image: url('/files/themes/clean-blog/photos/gallery/home-bg.jpg')">
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
