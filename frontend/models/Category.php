054a1666ea214e2411ba3c884d7e83f2a18a9ea1
Arunas Skucas <arsku77@gmail.com>
2017-06-30 09:47:40 +0000

    Urok15 shop models

---
+++ frontend/models/Category.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace frontend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "category".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $date_registered
+ * @property string $alias
+ */
+class Category extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'category';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['date_registered'], 'safe'],
+            [['name'], 'string', 'max' => 255],
+            [['alias'], 'string', 'max' => 55],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'date_registered' => 'Date Registered',
+            'alias' => 'Alias',
+        ];
+    }
+}
