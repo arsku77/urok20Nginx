9c437ab10180b7e7e1d52345011584713fd64651
Arunas Skucas <arsku77@gmail.com>
2017-08-04 05:45:20 +0000

    urok19 security issue in SQL

--- frontend/models/NewsSearch.php
+++ frontend/models/NewsSearch.php
@@ -18,7 +18,7 @@ class NewsSearch
     public function simpleSearch($keyword)
     {
         // TODO: fix security issue (hometask)
-
+        $keyword = strip_tags($keyword);
         $sql = "SELECT * FROM news WHERE content LIKE '%$keyword%' LIMIT 20";
         return Yii::$app->db->createCommand($sql)->queryAll();
     }
@@ -26,6 +26,7 @@ class NewsSearch
     public function fulltextSearch($keyword)
     {
         // TODO: fix security issue (hometask)
+        $keyword = strip_tags($keyword);
         $sql = "SELECT * FROM news WHERE MATCH (content) AGAINST ('$keyword') LIMIT 20";
         return Yii::$app->db->createCommand($sql)->queryAll();
     }
